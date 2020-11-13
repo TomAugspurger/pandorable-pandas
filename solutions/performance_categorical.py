6de3e7e2f2ccbe61918a2f9909aeccead5e17314
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-11-13 09:50:36 -0600

    Updates

--- solutions/performance_categorical.py
+++ solutions/performance_categorical.py
@@ -1,4 +1,5 @@
 
 character_bytes = 50 * sys.getsizeof("AL")  # Store all the states once
 bytes_per_person = 1  # np.int8 = 1 byte per element
-print("{:,d} MB".format((character_bytes + (population * bytes_per_person)) // 1_000_000))
+nbytes = (character_bytes + (population * bytes_per_person))
+print("{:,d} MB".format(nbytes // 1_000_000))
